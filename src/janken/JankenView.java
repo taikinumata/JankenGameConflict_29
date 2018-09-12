d9be7876915e51c8f7426eb7149bd83e78183f81
taiki numata <numata4171129@gmail.com>
2018-09-12 10:21:04 +0900

    first commit

---
+++ src/janken/JankenView.java
@@ -0,0 +1,12 @@
+package janken;
+
+import javafx.application.Application;
+
+public class JankenView extends Application {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
